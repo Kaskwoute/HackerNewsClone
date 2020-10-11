9c0de7b42ef731e59171f81ff59924d7e1da6b0b
Kaskwoute <antoine.peyrin@hotmail.fr>
2020-10-11 21:35:30 +0200

    add useRequest cache and can take array url param

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,17 +1,34 @@
-import React, { useEffect, useReducer } from 'react';
-
+import React, { useEffect, useReducer, useRef } from 'react';
 import { dataFetchReducer, fetchSuccess, fetchFailure, initFetch } from '../reducers/useResquest.reducer';
 
 /**
  * useRequest: Hook to make GET request
  *
  * @param initUrl: url to fetch from (only work for GET)
+ * @param cachable: boolean whether or not to cache this request
  * @return {{isLoading: boolean, isError: boolean, data: (Object | Array | undefined )}}
  */
-const useRequest = (initUrl) => {
+const useRequest = (initUrl, cachable = false) => {
+  const cache             = useRef({});
   const [state, dispatch] = useReducer(dataFetchReducer, dataFetchReducer());
 
+  const checkCachedUrls = (urlArray) => urlArray.reduce(({ cached, toFetch }, url) => {
+    if (cache.current[url]) cached = [...cached, cache.current[url]];
+    else toFetch = [...toFetch, url];
+
+    return {
+      cached: cached,
+      toFetch: toFetch,
+    }
+
+  }, {
+    cached: [],
+    toFetch: [],
+  });
+
   useEffect(() => {
+    if (!initUrl || (Array.isArray(initUrl) && initUrl.length === 0)) return;
+
     // Prevent changing state when unmounted (might use setInterval later on, if not remove it)
     let _isMounted = true;
 
@@ -19,9 +36,36 @@ const useRequest = (initUrl) => {
       try {
         dispatch(initFetch());
 
-        const response = await fetch(initUrl);
+        let responseData;
 
-        const responseData = await response.json();
+        if (Array.isArray(initUrl)) {
+
+          const { cached, toFetch } = checkCachedUrls(initUrl);
+
+           const data = await Promise.all(toFetch.map(
+            async (url) => {
+              const resp = await fetch(url);
+
+              const data =  await resp.json();
+
+              if(cachable) cache.current[url] = data;
+
+              return data
+            },
+          ));
+
+          responseData = [...data, ...cached]
+        } else {
+
+          if (cache.current[initUrl]) responseData = cache.current[initUrl];
+          else {
+            const response = await fetch(initUrl);
+
+            responseData = await response.json();
+
+            if (cachable) cache.current[initUrl] = responseData;
+          }
+        }
 
         if (_isMounted) dispatch(fetchSuccess(responseData));
       } catch (err) {
@@ -32,7 +76,7 @@ const useRequest = (initUrl) => {
     fetchData();
 
     return () => { _isMounted = false; }
-  }, [initUrl]);
+  }, [JSON.stringify(initUrl)]);
 
   return state;
 };
