ad3522b3f41c923e5736a3dc352ccb2bcccb7286
Kaskwoute <antoine.peyrin@hotmail.fr>
2020-10-11 12:20:38 +0200

    base navigation for app

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
 
+import { StoryContainer } from './container/StoryContainer';
+import { Text } from 'react-native';
+
+const Stack = createStackNavigator();
 
 const Navigation = () => {
-
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={ 'Home' }>
+        <Stack.Screen name={ 'Home' } component={ StoryContainer } options={ {
+          title: 'Hews',
+          headerStyle: styles.headerStyle,
+          headerTintColor: '#fff',
+        } }/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 };
 
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: '#fa8d00',
+  },
+});
+
 export {
-  Navigation
+  Navigation,
 }
